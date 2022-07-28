5e0e14e05f6a1fba10a649b6696dbd28d4b38c4e
d-brazeau <88905938+d-brazeau@users.noreply.github.com>
2022-07-28 17:29:48 -0400

    Add files via upload

--- MNItoVOXEL.m
+++ MNItoVOXEL.m
@@ -1,9 +1,11 @@
 
-%% MNI coord to voxel with ROAST
+%% MNI coordinates to voxel with ROAST
 
 %Steps prior to run
-% 1. load seg8.mat file which corresponds to the MRI used for the subject
-% 2. Create a matrix called targetCoord with the target coordinates
+% 1. run roast
+% 2. load seg8.mat file which corresponds to the MRI used for the subject
+% 3. Create a matrix called targetCoord with the target coordinates in MNI
+% format
 
 % the Affine matrix is loaded when you load seg8.mat
 
--- VOXELtoMNI.m
+++ VOXELtoMNI.m
@@ -1,12 +1,13 @@
 
-%% Voxel coord to mni with ROAST
+%% Voxel coordinates to mni with ROAST
 
 %Steps prior to run
-% 1. load seg8.mat file which corresponds to the MRI used for the subject
-% 2. Create a matrix called targetCoord with the target coordinates
+% 1. run roast
+% 2. load seg8.mat file which corresponds to the MRI used for the subject
+% 3. Create a matrix called targetCoord with the target coordinates in
+% subject space
 
 % the Affine matrix is loaded when you load seg8.mat
-
 
 mri2mni = Affine*(image(1).mat);
 % mapping from MNI space to individual MRI
